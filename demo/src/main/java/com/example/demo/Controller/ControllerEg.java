455167f5416fa9f421b30ff4cb43006d1b36c22a
Rohan <rohanthakare17699@gmail.com>
2023-03-11 20:56:25 +0530

    Rohan

--- demo/src/main/java/com/example/demo/Controller/ControllerEg.java
+++ demo/src/main/java/com/example/demo/Controller/ControllerEg.java
@@ -2,17 +2,14 @@ package com.example.demo.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.Entity.TestEntity;
 import com.example.demo.Service.EmailService;
 import com.example.demo.repo.TestRepo;
-
-import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ControllerEg 
